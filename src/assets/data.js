0198e1f001911e69a955ff463e516e15f8410ed9
Mayank Kumar <icodermayank@gmail.com>
2023-12-01 09:00:52 +0530

    Quiz data added.

---
+++ src/assets/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        question: "Which is required for the internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "Lan Cable",
+        option4: "Pen Drive",
+        ans: 1,
+
+    },
+    {
+        question:"Which is not a input device?",
+        option1:"Printer",
+        option2:"mouse",
+        option3:"joystick",
+        option3: "monitor",
+        ans:1,
+    },
+
+    {
+        question: "Which is not a programming language?",
+        option1:"java",
+        option2:"R",
+        option3:"css",
+        option4:"Ruby",
+        ans:3,
+    },
+
+    {
+        question: "Which is an object oriented programming language?",
+        option1:"SQL",
+        option2:"C",
+        option3:"React",
+        option4:"Python",
+        ans:4, 
+    },
+    {
+    question: "Which owns Android Os?",
+    option1:"Microsoft",
+    option2:"Google",
+    option3:"Amazon",
+    option4:"Meta",
+    ans:2,
+    }
+
+]
